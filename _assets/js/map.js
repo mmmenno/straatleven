e2e7e7e49b58ab75ab781f24dac3ff26de583216
Menno den Engelse <menno@denengelse.nl>
2023-12-06 09:05:34 +0100

    changed tiles, again

--- _assets/js/map.js
+++ _assets/js/map.js
@@ -76,11 +76,10 @@ function createMap(){
         position: 'bottomright'
     }).addTo(map);
 
-    overviewLayer = L.tileLayer('https://tiles.stadiamaps.com/tiles/stamen_toner_lite/{z}/{x}/{y}{r}.{ext}', {
-        minZoom: 0,
-        maxZoom: 20,
-        attribution: '&copy; <a href="https://www.stadiamaps.com/" target="_blank">Stadia Maps</a> &copy; <a href="https://www.stamen.com/" target="_blank">Stamen Design</a> &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-        ext: 'png'
+    overviewLayer = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/Canvas/World_Light_Gray_Base/MapServer/tile/{z}/{y}/{x}', {
+      attribution: 'Tiles &copy; Esri &mdash; Esri, DeLorme, NAVTEQ',
+      maxZoom: 16,
+      minZoom: 0
     }).addTo(map);
     
 
